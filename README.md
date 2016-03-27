08177672637ffad3cbe369a82dbe72b311b4f186
Frank van der Linden <flinden68@elstarit.nl>
2016-03-27 08:55:59 +0200

    Added plugin, feature and update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-cloudant-connector
+A basic cloudant-connector as a OSGi plugin which can be used on top of Domino
---
+++ nl.elstarit.cloudant/src/nl/elstarit/cloudant/test/CloudantTester.java
@@ -0,0 +1,10 @@
+package nl.elstarit.cloudant.test;
+
+public class CloudantTester {
+
+	public CloudantTester(){
+		System.out.println("Start testing the cloudant connector");
+	}
+
+
+}
